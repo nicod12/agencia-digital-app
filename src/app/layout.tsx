507dd3b5de1275811d8c3b33c6cbaea03c7da1eb
nicod12 <dumenicolas45@gmail.com>
2024-02-20 07:01:18 -0300

    udp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className} suppressHydrationWarning={true}>
         <main>
-          <section className="container mx-auto items-center g px-8 xl:px-2 2xl:px-2">
+          <section className="container mx-auto h-screen items-center g px-8 xl:px-2 2xl:px-2">
             <Navbar />
             {children}
           </section>
