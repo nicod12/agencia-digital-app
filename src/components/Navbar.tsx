d5abb8ca38ad94d4f298a43a7e7cc21763e99da0
nicod12 <dumenicolas45@gmail.com>
2024-02-23 12:46:21 -0300

    upd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
                 exit={{ opacity: 0 }}
                >
                    <div className="flex flex-col">
-                   <ul className={`grid grid-cols-1 items-center justify-center text-center text-[15px] sm:text-2xl md:sm:text-2xl px-10 py-4 ${poppins.className} uppercase`}>
+                   <ul className={`grid grid-cols-1 items-center justify-center text-center text-[12px] sm:text-2xl md:sm:text-2xl px-10 py-4 ${poppins.className} uppercase`}>
                     {links.map((link, index) => (
                         <li key={index} className={`my-6 sm:my-4 sm:py-4 border-b border-[#96449B] hover:bg-[#96449B] hover:rounded hover:text-slate-200 hover:transition hover:duration-200 hover:ease-in-out`}>
                             <ScrollLink
