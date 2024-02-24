1147e0c19021fc77a94987a3d431267c69f55cee
nicod12 <dumenicolas45@gmail.com>
2024-02-24 12:00:37 -0300

    udp

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: 'i.postimg.cc',
-              port: '',
-              
-          },
-      ],
-        },
+
 };
 
 export default nextConfig;
